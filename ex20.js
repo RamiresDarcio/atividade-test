8e8180ab1a569a25ee179525e49f2e0615472add
ramires_felino <ramiresdarcio2104@gmail.com>
2024-11-11 16:25:51 -0300

    atividade do curso na sala de aula
    sdasd

---
+++ ex20.js
@@ -0,0 +1,6 @@
+let anoNacimentor =2004;
+let anoAltual =2025;
+/*essa são varialvel da a pa processesamento */ 
+const suaidade = anoNacimentor - anoAltual;
+alert("a sua idade  é" + suaidade); 
+// esse processametor do resutador da atividade //
